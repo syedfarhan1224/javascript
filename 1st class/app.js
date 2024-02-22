a7131e51eed3a810ad59ffbfd606093fed7d00db
Syed Farhan <80678813+syedfarhan1224@users.noreply.github.com>
2024-02-22 16:18:44 +0500

    first commit

---
+++ 1st class/app.js
@@ -0,0 +1,9 @@
+// Q:1&2
+// alert("Error! Please enter a valid password");
+// Q:3
+// alert("Wellcome to js land... \n Happy codeing!");
+// Q:4
+// alert("Welcome to js land...");
+// alert("Happy codeing! \n check boxs");
+// Q:5
+// alert("Hello... I can run js through my web browser's console");
---
+++ 2nd class/app.js
@@ -0,0 +1,26 @@
+// Q:1&2
+// alert("Error! Please enter a valid password");
+// Q:3
+// alert("Wellcome to js land... \n Happy codeing!");
+// Q:4
+// alert("Welcome to js land...");
+// alert("Happy codeing! \n check boxs");
+// Q:5
+// alert("Hello... I can run js through my web browser's console");
+// chapter:02
+// ex.1-4
+var username = "farhan1224";
+var myname = "Syed Farhan";
+var message = "Certified Mobile Application Development";
+alert("user name is " + username);
+alert("Full name is " + myname);
+alert(message);
+
+// ex.5
+var p1 = "PIZZA";
+var p2 = "PIZZ";
+var p3 = "PIZ";
+var p4 = "PI";
+var p5 = "P";
+
+alert(p1 + "\n" + p2 + "\n" + p3 + "\n" + p4 + "\n" + p5);
