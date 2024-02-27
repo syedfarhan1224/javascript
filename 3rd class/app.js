c1a335134bbfa4ef9c73f9da5382d3fc4000cff9
Syed Farhan <80678813+syedfarhan1224@users.noreply.github.com>
2024-02-27 17:06:52 +0500

    js updated

--- 3rd class/app.js
+++ 3rd class/app.js
@@ -1,33 +1,49 @@
-// Q:1&2
-// alert("Error! Please enter a valid password");
-// Q:3
-// alert("Wellcome to js land... \n Happy codeing!");
-// Q:4
-// alert("Welcome to js land...");
-// alert("Happy codeing! \n check boxs");
-// Q:5
-// alert("Hello... I can run js through my web browser's console");
-// chapter:02
-// ex.1-4
-// var username = "farhan1224";
-// var myname = "Syed Farhan";
-// var message = "Certified Mobile Application Development";
-// alert("user name is " + username);
-// alert("Full name is " + myname);
-// alert(message);
+// // Q:1&2
+// // alert("Error! Please enter a valid password");
+// // Q:3
+// // alert("Wellcome to js land... \n Happy codeing!");
+// // Q:4
+// // alert("Welcome to js land...");
+// // alert("Happy codeing! \n check boxs");
+// // Q:5
+// // alert("Hello... I can run js through my web browser's console");
+// // chapter:02
+// // ex.1-4
+// // var username = "farhan1224";
+// // var myname = "Syed Farhan";
+// // var message = "Certified Mobile Application Development";
+// // alert("user name is " + username);
+// // alert("Full name is " + myname);
+// // alert(message);
 
-// // ex.5
-// var p1 = "PIZZA";
-// var p2 = "PIZZ";
-// var p3 = "PIZ";
-// var p4 = "PI";
-// var p5 = "P";
+// // // ex.5
+// // var p1 = "PIZZA";
+// // var p2 = "PIZZ";
+// // var p3 = "PIZ";
+// // var p4 = "PI";
+// // var p5 = "P";
 
-// alert(p1 + "\n" + p2 + "\n" + p3 + "\n" + p4 + "\n" + p5);
+// // alert(p1 + "\n" + p2 + "\n" + p3 + "\n" + p4 + "\n" + p5);
 
-// chapter no : 03
-var age = 22;
-alert("I am " + age + " years old");
+// // chapter no : 03
+// var age = 22;
+// alert("I am " + age + " years old");
 
-var times = 17;
-alert("you have visited this site  " + times + " time");
+// var times = 17;
+// alert("you have visited this site  " + times + " time");
+// Q=1
+var age = 23;
+alert(age);
+
+//Q=2
+var times = "You have visited this site 10 times";
+alert(times);
+
+//Q=3
+var birth = "My birth year is 2000";
+var data = "Data type is Variable Nomber";
+document.write(birth + "</br>" + data + "</br>");
+
+//Q=4
+var q4 = "John Doe ordered 5 T-shirt(s) on XYZ Clothing store";
+document.write(q4);
--- 4th class/app.js
+++ 4th class/app.js
@@ -1,37 +1,73 @@
-// Q:1&2
-// alert("Error! Please enter a valid password");
-// Q:3
-// alert("Wellcome to js land... \n Happy codeing!");
-// Q:4
-// alert("Welcome to js land...");
-// alert("Happy codeing! \n check boxs");
-// Q:5
-// alert("Hello... I can run js through my web browser's console");
-// chapter:02
-// ex.1-4
-// var username = "farhan1224";
-// var myname = "Syed Farhan";
-// var message = "Certified Mobile Application Development";
-// alert("user name is " + username);
-// alert("Full name is " + myname);
-// alert(message);
+// // Q:1&2
+// // alert("Error! Please enter a valid password");
+// // Q:3
+// // alert("Wellcome to js land... \n Happy codeing!");
+// // Q:4
+// // alert("Welcome to js land...");
+// // alert("Happy codeing! \n check boxs");
+// // Q:5
+// // alert("Hello... I can run js through my web browser's console");
+// // chapter:02
+// // ex.1-4
+// // var username = "farhan1224";
+// // var myname = "Syed Farhan";
+// // var message = "Certified Mobile Application Development";
+// // alert("user name is " + username);
+// // alert("Full name is " + myname);
+// // alert(message);
 
-// // ex.5
-// var p1 = "PIZZA";
-// var p2 = "PIZZ";
-// var p3 = "PIZ";
-// var p4 = "PI";
-// var p5 = "P";
+// // // ex.5
+// // var p1 = "PIZZA";
+// // var p2 = "PIZZ";
+// // var p3 = "PIZ";
+// // var p4 = "PI";
+// // var p5 = "P";
 
-// alert(p1 + "\n" + p2 + "\n" + p3 + "\n" + p4 + "\n" + p5);
+// // alert(p1 + "\n" + p2 + "\n" + p3 + "\n" + p4 + "\n" + p5);
 
-// chapter no : 03
-// var age = 22;
-// alert("I am " + age + " years old");
+// // chapter no : 03
+// // var age = 22;
+// // alert("I am " + age + " years old");
 
-// var times = 17;
-// alert("you have visited this site  " + times + " time");
+// // var times = 17;
+// // alert("you have visited this site  " + times + " time");
 
-// chapter no : 4
-var ch4;
-alert(ch4);
+// // chapter no : 4
+// var ch4;
+// alert(ch4);
+
+// Q=1
+var Name = "Syed Farhan";
+var age = 22;
+var ed = "Student";
+document.write(Name + "</br>" + age + "</br>" + ed);
+
+// illegal
+// var @myname = "Fahad Qureshi";
+// var 123myName = "Fahad Qureshi";
+// var my name = "Fahad Qureshi";
+// var month&year = "Fahad Qureshi";
+// var birth-year = "Fahad Qureshi";
+
+// Q=3
+var heading = "Rules for naming JS variables";
+var a =
+  "Variable name can contain only letters, numbers, $, and underscores. \nFor example $my_1stVariable";
+var b =
+  "Variables must begin with a letter, $ or _ \n. For example $name, _name or name";
+var c = "Variable names are case Sensitive";
+var d = "Variable names should not be JS variable";
+document.write(
+  "</br>" +
+    heading +
+    "</br>" +
+    "</br>" +
+    "</br>" +
+    a +
+    "</br>" +
+    b +
+    "</br>" +
+    c +
+    "</br>" +
+    d
+);
