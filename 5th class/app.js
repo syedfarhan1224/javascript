d8d4c65375cdb7e5db7a366b25edf26dc57651ae
Syed Farhan <80678813+syedfarhan1224@users.noreply.github.com>
2024-02-29 00:02:19 +0500

    chapter 05 assignment done

---
+++ 5th class/app.js
@@ -0,0 +1,105 @@
+// Q no 1
+// Given var x = 25 and var y = 7, what is the result of x % y?
+var x = 25;
+var y = 7;
+var z = x % y;
+console.log(z);
+
+// Q no 2
+// If var a = 15 and var b = 4, what is the value of a * b after adding 10 to a and subtracting 2 from b?
+var a = 15 + 10;
+var b = 4 - 2;
+
+var c = a * b;
+console.log(c);
+
+// Q no 3
+// Suppose var c = 12 and var d = 3, calculate the result of c ** d.
+var c = 12;
+var d = 3;
+var z = c ** d;
+console.log(z);
+
+// Q no 4
+// For var e = 10 and var f = 3, what is the value of e += f * 2?
+var e = 10;
+var f = 3;
+var z = (e += f * 2);
+console.log(z);
+
+// Q no 5
+// Considering var g = 20 and var h = 4, what will be the outcome of g / h after adding 5 to g?
+var g = 20 + 5;
+var h = 4;
+var z = g / h;
+console.log(z);
+
+// Q no 6
+// Given var i = 8 and var j = 3, determine the value of i * j after subtracting 2 from i.
+var i = 8 - 2;
+var j = 3;
+var z = i * j;
+console.log(z);
+
+// Q no 7
+// If var k = 18 and var l = 5, what is the result of k - l + 2?
+var k = 18;
+var l = 5;
+var z = k - l + 2;
+console.log(z);
+
+// Q no 8
+// Suppose var m = 7 and var n = 2, calculate the value of m ** n.
+var m = 7;
+var n = 2;
+var z = m ** n;
+console.log(z);
+
+// Q no 9
+// For var o = 13 and var p = 4, what will be the outcome of o % (p + 2)?
+var o = 13;
+var p = 4;
+var z = o % (p + 2);
+console.log(z);
+
+// Q no 10
+// Given var q = 16 and var r = 3, determine the result of q * r after dividing q by 2.
+var q = 16 / 2;
+var r = 3;
+var z = q * r;
+console.log(z);
+
+// Q no 11
+// If var s = 25 and var t = 3, what is the value of s / t after subtracting 1 from s?
+var s = 25 - 1;
+var t = 3;
+var z = s / t;
+console.log(z);
+
+// Q no 12
+// Suppose var u = 11 and var v = 3, calculate the value of u - v after adding 2 to v.
+var u = 11;
+var v = 3 + 2;
+var z = u - v;
+console.log(z);
+
+// Q no 13
+// For var w = 22 and var x = 5, what will be the outcome of w + (x ** 2)?
+var w = 22;
+var x = 5;
+var z = w + x ** 2;
+console.log(z);
+
+// Q no 14
+// Given var y = 19 and var z = 4, determine the result of y % (z + 1)
+var y = 19;
+var z = 4;
+var x = y % (z + 1);
+console.log(x);
+
+// Q no 15
+//  If var a = 10 and var b = 3, what is the value of a * (b - 2)?
+var a = 10;
+var b = 3;
+var z = a * (b - 2);
+console.log(z);
