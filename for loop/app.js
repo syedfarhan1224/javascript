7272ed40249e6d5a648d47e8ba6c91da52e454a4
Syed Farhan <80678813+syedfarhan1224@users.noreply.github.com>
2024-06-01 15:26:05 +0500

    interface was updated

---
+++ for loop/app.js
@@ -0,0 +1,39 @@
+//print from 1 to 10
+// var num1 = 0;
+// // var num2 = 1;
+
+// for (var i = 0; i < 10; i++) {
+//   num1 = i - num1;
+//   console.log(num1);
+// }
+
+//print square from 1 to 10
+for (var i = 1; i < 10; i++) {
+  console.log(i, 10 - i);
+}
+
+//print even numbers only
+// for (var i = 1; i <= 20; i++) {
+//   if (i % 2 == 0) {
+//     console.log(i);
+//   }
+// }
+
+//print odd numbers only
+// for (var i = 1; i <= 15; i++) {
+//   if (i % 2 == 1) {
+//     console.log(i);
+//   }
+// }
+
+// //print table of 4 from 1 to 10
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i * 4);
+// }
+
+// Write a loop to calculate and print the factorial of a number (let's say 5).
+// var factorial = prompt("enter a number for factorial");
+// for (var i = 1; i <= factorial; i++) {
+//   document.write(factorial * i);
+//   console.log(i);
+// }
